afd90748c3f7d346011c26a67f4a43a43f60e5a8
Igor <ig.vp@hotmail.com>
2023-01-23 19:00:48 -0300

    animation to SideMenu done

--- src/pages/SideMenu.jsx
+++ src/pages/SideMenu.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 const SideMenu = () => {
   const [open, setOpen] = useState(false);
   const [category, setCategory] = useState("Todos");
@@ -13,22 +13,29 @@ const SideMenu = () => {
   return (
     <main className="flex h-screen w-screen items-center justify-center bg-slate-300">
       <div className=" flex h-3/6 w-6/12 items-center justify-center p-5 px-10">
-        {open && (
-          <div className="h-full w-1/4 rounded-l-md bg-slate-800 pt-8">
-            {caterories.map((item, index) => {
-              return (
-                <div
-                  className={`flex h-10 w-full items-center px-3 ${
-                    item == category && "bg-white/20"
-                  } font-normal text-gray-100`}
-                  key={index}
-                >
-                  {item}
-                </div>
-              );
-            })}
-          </div>
-        )}
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ width: 0 }}
+              animate={{ width: "25%" }}
+              exit={{ width: 0, backgroundColor: "#FFF" }}
+              className="h-full rounded-l-md bg-slate-800 pt-8"
+            >
+              {caterories.map((item, index) => {
+                return (
+                  <div
+                    className={`flex h-10 w-full items-center px-3 ${
+                      item == category && "bg-white/20"
+                    } text-base font-normal text-gray-100`}
+                    key={index}
+                  >
+                    {item}
+                  </div>
+                );
+              })}
+            </motion.div>
+          )}
+        </AnimatePresence>
         <div
           className={`${
             open ? "rounded-r-md" : "rounded-md"
@@ -41,12 +48,30 @@ const SideMenu = () => {
           <div className=" flex h-full w-full flex-col  ">
             {tasks.map((item, index) => {
               return (
-                <div
-                  key={index}
-                  className="flex h-1/6 w-full items-center border-b-[1px] border-slate-800/10"
-                >
-                  <h1 className="text-lg font-normal text-slate-800">{item}</h1>
-                </div>
+                <AnimatePresence>
+                  <motion.div
+                    variants={{
+                      visible: (index) => ({
+                        opacity: 1,
+                        y: 0,
+                        transition: { delay: index * 0.05 },
+                      }),
+                      hidden: (index) => ({
+                        opacity: 0,
+                        y: -50 * index,
+                      }),
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    custom={index}
+                    key={index}
+                    className="flex h-1/6 w-full items-center border-b-[1px] border-slate-800/10"
+                  >
+                    <h1 className="text-lg font-normal text-slate-800">
+                      {item}
+                    </h1>
+                  </motion.div>
+                </AnimatePresence>
               );
             })}
           </div>
