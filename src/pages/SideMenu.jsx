6e801fcae0a49e4b4d09ac6e247da4b5f100f6db
Igor <ig.vp@hotmail.com>
2023-01-23 18:14:18 -0300

    sidemenu done

--- src/pages/SideMenu.jsx
+++ src/pages/SideMenu.jsx
@@ -1,11 +1,34 @@
 import { useState } from "react";
-
+import { motion } from "framer-motion";
 const SideMenu = () => {
   const [open, setOpen] = useState(false);
+  const [category, setCategory] = useState("Todos");
+  const [caterories, setCategories] = useState(["Todos", "Contas", "Lavar"]);
+  const [tasks, setTasks] = useState([
+    "Limpar o carro",
+    "Lavar louça",
+    "Pagar a internet",
+    "Pagar a faculdade",
+  ]);
   return (
     <main className="flex h-screen w-screen items-center justify-center bg-slate-300">
       <div className=" flex h-3/6 w-6/12 items-center justify-center p-5 px-10">
-        {open && <div className="h-full w-1/4 rounded-l-md bg-slate-800"></div>}
+        {open && (
+          <div className="h-full w-1/4 rounded-l-md bg-slate-800 pt-8">
+            {caterories.map((item, index) => {
+              return (
+                <div
+                  className={`flex h-10 w-full items-center px-3 ${
+                    item == category && "bg-white/20"
+                  } font-normal text-gray-100`}
+                  key={index}
+                >
+                  {item}
+                </div>
+              );
+            })}
+          </div>
+        )}
         <div
           className={`${
             open ? "rounded-r-md" : "rounded-md"
@@ -15,7 +38,18 @@ const SideMenu = () => {
             <h1 className="text-3xl font-bold text-slate-800"> To do</h1>
             <button className="font-Inter text-3xl text-slate-600">+</button>
           </div>
-          <div className=" h-full w-full bg-red-100 "></div>
+          <div className=" flex h-full w-full flex-col  ">
+            {tasks.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex h-1/6 w-full items-center border-b-[1px] border-slate-800/10"
+                >
+                  <h1 className="text-lg font-normal text-slate-800">{item}</h1>
+                </div>
+              );
+            })}
+          </div>
           <div className=" group absolute left-0 top-1/2 flex h-full w-8 -translate-y-1/2 items-center justify-center  ">
             <button
               className=" hidden h-16 w-[10px] rounded-full bg-slate-200 hover:border-2 hover:border-slate-400 group-hover:flex"
