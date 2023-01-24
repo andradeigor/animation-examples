ce8178a15f9fbe0ac37b7ff0b3375fe04deb5b54
Igor <ig.vp@hotmail.com>
2023-01-24 15:47:19 -0300

    carossel started

---
+++ src/pages/Carossel.jsx
@@ -0,0 +1,7 @@
+const Carossel = () => {
+  return (
+    <main className="flex min-h-screen w-screen items-center justify-center bg-zinc-300"></main>
+  );
+};
+
+export default Carossel;
--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import MailBox from "./pages/MailBox";
 import MultiStep from "./pages/MultiStep";
 import SideMenu from "./pages/SideMenu";
+import Carossel from "./pages/Carossel";
 
 export const Router = createBrowserRouter([
   { path: "/multistep", element: <MultiStep /> },
   { path: "/sidemenu", element: <SideMenu /> },
   { path: "/mailbox", element: <MailBox /> },
+  { path: "/carossel", element: <Carossel /> },
 ]);
