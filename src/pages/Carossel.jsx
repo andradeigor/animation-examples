202c820483da5c722ba67a362d7cf8a8e901cf3e
Igor <ig.vp@hotmail.com>
2023-01-24 19:06:18 -0300

    carossel animation done

--- src/pages/Carossel.jsx
+++ src/pages/Carossel.jsx
@@ -6,6 +6,8 @@ import Image5Path from "../assets/image5.jpg";
 import Image6Path from "../assets/image6.jpg";
 import { ArrowSmallRightIcon } from "../components/RightArrowIcon/RightArrowIcon";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 const Carossel = () => {
   const [Images, setImages] = useState([
     Image1Path,
@@ -15,32 +17,55 @@ const Carossel = () => {
     Image5Path,
     Image6Path,
   ]);
+
   const [image, setImage] = useState(0);
+  const [prev, setPrev] = useState(image);
+  let direction = image > prev ? 1 : -1;
   const HandleRightClick = () => {
     if (image >= Images.length - 1) {
       return;
     }
+    setPrev(image);
     setImage(image + 1);
   };
   const HandleLeftClick = () => {
     if (image <= 0) {
       return;
     }
+    setPrev(image);
     setImage(image - 1);
   };
   return (
-    <main className="flex h-screen w-screen  bg-gray-50">
-      <div className=" relative h-3/4 w-full">
-        <img src={Images[image]} className=" h-full w-full object-cover" />
+    <main className="h-screen w-screen  bg-gray-50">
+      <div className=" relative flex h-full w-full items-center justify-center">
+        <AnimatePresence initial={false}>
+          <motion.img
+            className="absolute h-full w-full select-none object-cover "
+            src={Images[image]}
+            variants={{
+              enter: (direction) => ({
+                x: 1000 * direction,
+              }),
+              center: { x: 0 },
+              exit: (direction) => ({ x: -1000 * direction }),
+            }}
+            custom={direction}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            key={image}
+          />
+        </AnimatePresence>
+
         <div
           onClick={HandleRightClick}
-          className="absolute top-1/2 right-0 flex h-12 w-12 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full  bg-zinc-900/40"
+          className="absolute top-1/2 right-2 flex h-12 w-12 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full  bg-zinc-900/40"
         >
           <ArrowSmallRightIcon className=" h-6 w-6 font-bold text-gray-50" />
         </div>
         <div
           onClick={HandleLeftClick}
-          className="absolute top-1/2 left-0 flex h-12 w-12 -translate-y-1/2 rotate-180 cursor-pointer items-center justify-center rounded-full  bg-zinc-900/40"
+          className="absolute top-1/2 left-2 flex h-12 w-12 -translate-y-1/2 rotate-180 cursor-pointer items-center justify-center rounded-full  bg-zinc-900/40"
         >
           <ArrowSmallRightIcon className=" h-6 w-6 font-bold text-gray-50" />
         </div>
